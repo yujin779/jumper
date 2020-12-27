caf93463d6fbb5a96f19bc02d2271ec10b7fe873
yujin779 <51077021+yujin779@users.noreply.github.com>
2020-12-27 22:41:10 +0900

    useCannon

--- src/components/Floor.js
+++ src/components/Floor.js
@@ -2,20 +2,24 @@ import React, { useRef, useState } from "react";
 import * as THREE from "three";
 import { View, Text } from "react-native";
 import { Canvas, useFrame, useThree, extend } from "react-three-fiber";
-
+import { Physics, useBox, usePlane, useSphere } from "use-cannon";
 import { createGlobalState } from "react-hooks-global-state";
 
 /*
  * 1. 表示される入り口
  */
 const Floor = () => {
+  const [ref] = useBox(() => ({
+    type: "Static",
+    mass: 1,
+    args: [10, 1, 10],
+    position: [0, -1, 0]
+  }));
   return (
-    <group>
-      <mesh position={[0, -1, 0]}>
-        <boxBufferGeometry attach="geometry" args={[10, 1, 10]} />
-        <meshStandardMaterial color={"hotpink"} />
-      </mesh>
-    </group>
+    <mesh ref={ref}>
+      <boxBufferGeometry attach="geometry" args={[10, 1, 10]} />
+      <meshStandardMaterial attach="material" color={"hotpink"} />
+    </mesh>
   );
 };
 export default Floor;
--- src/components/Player.js
+++ src/components/Player.js
@@ -2,20 +2,23 @@ import React, { useRef, useState } from "react";
 import * as THREE from "three";
 import { View, Text } from "react-native";
 import { Canvas, useFrame, useThree, extend } from "react-three-fiber";
-
+// import * as CANNON from "cannon";
 import { createGlobalState } from "react-hooks-global-state";
-
+import { Physics, useBox, usePlane, useSphere } from "use-cannon";
 /*
  * 1. 表示される入り口
  */
 const Player = () => {
+  const [ref] = useBox(() => ({
+    mass: 1,
+    args: [1, 1, 1],
+    position: [0, 3, 0],
+  }));
   return (
-    <group>
-      <mesh position={[0, 0, 0]}>
-        <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-        <meshStandardMaterial color={"orange"} />
-      </mesh>
-    </group>
+    <mesh ref={ref}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial attach="material" color={"orange"} />
+    </mesh>
   );
 };
 export default Player;
